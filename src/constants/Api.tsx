f8c6aae6cfa76e51cd4bd9cfd1922df60152264f
ayush <ayush.bajaj@autviz.in>
2026-08-19 15:22:57 +0530

    feat: implement FCM token deletion on logout and update application versioning and display name

--- src/constants/Api.tsx
+++ src/constants/Api.tsx
@@ -109,3 +109,12 @@ export const updateFcmToken = async (fcmToken: string) => {
     })
     return response.data
 }
+
+// delete FCM token
+export const deleteFcmToken = async (fcmToken: string) => {
+    const response = await axios.post('/fcm/delete-token', {
+        fcmToken: fcmToken
+    })
+    return response.data
+}
+
